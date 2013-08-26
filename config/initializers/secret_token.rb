7f168f29bb435f1eec4faedb8e578f9713cd748b
Roman <broilerster@gmail.com>
2013-08-26 13:58:56 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mealprice::Application.config.secret_key_base = 'b524f01854cbe0fa98a2506de25552bfb3f5bda6f08149801cbb868b1e38c785e1c89cf8dc664d7ba270f1113645c1954f577adb7d6343aa60f7b6b3f86b2ba8'
